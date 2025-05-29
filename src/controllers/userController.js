e377347d0b8f227c6a3d1736a80d004063508441
Lorety-VL <vladimirking18@gmail.com>
2025-05-29 23:24:11 +0300

    fix error where id in /users/:id is not valid id

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,6 +26,10 @@ class UserController {
 
   async getUser(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest('Validation error', errors.array()))
+      }
       const { id } = req.params;
       const lawyer = await userService.getById(id);
 
